b05a6c9c5d9cfbbf338f7c222dc4b99eefc973a6
ahmadhdr <ahmadhdr.22@gmail.com>
2019-11-21 15:50:11 +0700

    function as type

--- build/function.js
+++ build/function.js
@@ -6,9 +6,12 @@
 //     console.log('print name')
 // }
 // console.log(getName())
+// argument types
 function multiply(a, b) {
     return a * b;
 }
-var result = multiply(5, 3);
-console.log(result);
-// printName()
+var Add = function (val1, val2) {
+    return val1 * val2;
+};
+console.log(Add(2, 3));
+// printName()git
--- src/function.ts
+++ src/function.ts
@@ -8,14 +8,26 @@
 // }
 // console.log(getName())
 
+
+// argument types
 function multiply (a : number,b : number) : number {
     return a * b
 }
 
-let result = multiply(5,3)
+// let result = multiply(5,3)
+type age = number
+// console.log(result)
 
-console.log(result)
 
+// function as type
 
-// printName()
+type Tambah = (val1 : number, val2 : number) => number
+
+const Add : Tambah  = (val1 : number,val2:number) => {
+    return val1 * val2
+}
+
+console.log(Add(2,3))
+
+// printName()git
 
