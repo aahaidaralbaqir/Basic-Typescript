b21b65934d77c6dcc62eeddc3134d9f356db40dc
ahmadhdr <ahmadhdr.22@gmail.com>
2019-11-21 14:44:22 +0700

    function

--- build/function.js
+++ build/function.js
@@ -1,9 +1,14 @@
 "use strict";
-function getName() {
-    return "hello, my name is ahmad haidar";
-}
-function printName() {
-    console.log('print name');
-}
+// function getName() : string {
+//     return "hello, my name is ahmad haidar"
+// }
+// function printName() : void{
+//     console.log('print name')
+// }
 // console.log(getName())
-printName();
+function multiply(a, b) {
+    return a * b;
+}
+var result = multiply(5, 3);
+console.log(result);
+// printName()
--- src/function.ts
+++ src/function.ts
@@ -1,10 +1,21 @@
-function getName() : string {
-    return "hello, my name is ahmad haidar"
-}
+// function getName() : string {
+//     return "hello, my name is ahmad haidar"
+// }
 
 
-function printName() : void{
-    console.log('print name')
-}
+// function printName() : void{
+//     console.log('print name')
+// }
 // console.log(getName())
-printName()
+
+function multiply (a : number,b : number) : number {
+    return a * b
+}
+
+let result = multiply(5,3)
+
+console.log(result)
+
+
+// printName()
+
